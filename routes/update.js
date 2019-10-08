898a6179d18a8bef6736c5ea9a31cb43cb13a2de
ShaunLWM <shaunlimweimin@gmail.com>
2019-10-08 11:37:11 +0800

    Refactoring

--- routes/update.js
+++ routes/update.js
@@ -11,11 +11,9 @@ router.put("/*", [Utils.validateUrl, Utils.ensureBody, Utils.ensureJson, Utils.v
     let id = req._id;
     let collection = req._collection;
     if (typeof id === "undefined") return next("id must be provided to update")
-    console.log(key, id)
     let data = await database.get(key, req);
     if (data.length === 0) return next("id in bin does not exist");
     if (data.length > 1) return next("id has multiple data. please contact admin");
-    // let updatedData = Object.assign(data[0], req.body);
     await database.delete({ key, collection, id });
     let p = Utils.formatDatabaseJson(data[0], req.body, true);
     await database.add(key, p);
